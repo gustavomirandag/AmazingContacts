62fd5bf16db1a2c97bf02a197ef9f78fcf05ca72
Gustavo de Miranda <gustavolpu@hotmail.com>
2020-11-09 22:07:01 -0300

    Added a Desktop version of the Amazing Contacts.

---
+++ AmazingContacts.DesktopApp/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace AmazingContacts.DesktopApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBoxContacts = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxPhone = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonRegisterContact = new System.Windows.Forms.Button();
+            this.textBoxSearchByName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBoxSearchByName);
+            this.groupBox1.Controls.Add(this.listBoxContacts);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(387, 324);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Contacts List";
+            // 
+            // listBoxContacts
+            // 
+            this.listBoxContacts.FormattingEnabled = true;
+            this.listBoxContacts.ItemHeight = 16;
+            this.listBoxContacts.Location = new System.Drawing.Point(12, 64);
+            this.listBoxContacts.Name = "listBoxContacts";
+            this.listBoxContacts.Size = new System.Drawing.Size(364, 244);
+            this.listBoxContacts.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.buttonRegisterContact);
+            this.groupBox2.Controls.Add(this.textBoxAddress);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.textBoxEmail);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.textBoxPhone);
+            this.groupBox2.Controls.Add(this.textBoxName);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(405, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(312, 324);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Register Contact";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Phone";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(89, 71);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(206, 22);
+            this.textBoxName.TabIndex = 2;
+            // 
+            // textBoxPhone
+            // 
+            this.textBoxPhone.Location = new System.Drawing.Point(89, 107);
+            this.textBoxPhone.Name = "textBoxPhone";
+            this.textBoxPhone.Size = new System.Drawing.Size(206, 22);
+            this.textBoxPhone.TabIndex = 3;
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(89, 150);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(206, 22);
+            this.textBoxEmail.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Email";
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Location = new System.Drawing.Point(89, 193);
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(206, 22);
+            this.textBoxAddress.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 197);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Address";
+            // 
+            // buttonRegisterContact
+            // 
+            this.buttonRegisterContact.Location = new System.Drawing.Point(89, 254);
+            this.buttonRegisterContact.Name = "buttonRegisterContact";
+            this.buttonRegisterContact.Size = new System.Drawing.Size(152, 35);
+            this.buttonRegisterContact.TabIndex = 8;
+            this.buttonRegisterContact.Text = "Register Contact";
+            this.buttonRegisterContact.UseVisualStyleBackColor = true;
+            this.buttonRegisterContact.Click += new System.EventHandler(this.buttonRegisterContact_Click);
+            // 
+            // textBoxSearchByName
+            // 
+            this.textBoxSearchByName.Location = new System.Drawing.Point(132, 29);
+            this.textBoxSearchByName.Name = "textBoxSearchByName";
+            this.textBoxSearchByName.Size = new System.Drawing.Size(244, 22);
+            this.textBoxSearchByName.TabIndex = 10;
+            this.textBoxSearchByName.TextChanged += new System.EventHandler(this.textBoxSearchByName_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 32);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(112, 16);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Search by Name:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 345);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Amazing Contacts";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBoxContacts;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button buttonRegisterContact;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxPhone;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxSearchByName;
+    }
+}
+
---
+++ AmazingContacts.DesktopApp/Form1.cs
@@ -0,0 +1,61 @@
+﻿using AmazingContacts.DesktopApp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AmazingContacts.DesktopApp
+{
+    public partial class Form1 : Form
+    {
+        //Represents our Database
+        private static List<Contact> Contacts = new List<Contact>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonRegisterContact_Click(object sender, EventArgs e)
+        {
+            var contact = new Contact();
+            contact.Name = textBoxName.Text;
+            contact.Phone = textBoxPhone.Text;
+            contact.Email = textBoxEmail.Text;
+            contact.Address = textBoxAddress.Text;
+
+            Contacts.Add(contact);
+
+            ListAllContacts(Contacts);
+
+            //Apagar Campos
+            textBoxName.Text = "";
+            textBoxPhone.Text = "";
+            textBoxEmail.Text = "";
+            textBoxAddress.Text = "";
+        }
+
+        private void ListAllContacts(List<Contact> contacts)
+        {
+            listBoxContacts.Items.Clear();
+
+            foreach(var contact in contacts)
+            {
+                listBoxContacts.Items.Add($"{contact.Name} - {contact.Phone}");
+            }
+        }
+
+        private void textBoxSearchByName_TextChanged(object sender, EventArgs e)
+        {
+            var name = ((TextBox)sender).Text;
+            var searchResult = Contacts.Where(contact => contact.Name.ToLower().Contains(name.ToLower())).ToList();
+
+            ListAllContacts(searchResult);
+        }
+    }
+}
